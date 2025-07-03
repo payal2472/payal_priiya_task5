c7b291a1dcf0177dc52432b47c7c0f9d60a5d911
payal priya <payalpriya8360@gmail.com>
2025-07-03 22:41:44 +0530

    add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 app.get('/hello', (req, res) => {
-    res.status(200).json({msg:"hello"})
+    res.status(200).json({msg:"hello world"})
 });
+const 
 
